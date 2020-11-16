abce4fd242e90a6ce1825f3183581e3c4de6d8db
Laith-Kamaraldin <laithkamaral@gmail.com>
2020-11-16 23:37:39 +0000

    changed config file to fix github pages prefix error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/myPortfolio",
   siteMetadata: {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
@@ -134,7 +135,8 @@ module.exports = {
       },
     },
 
- 
+    
+
     
     {
       resolve: `gatsby-plugin-manifest`,
@@ -148,8 +150,5 @@ module.exports = {
         icon: `src/images/icon.png`,
       },
     },
-    {
-    pathPrefix: "/myPortfolio",
-    },
-  ],
+    ],
 };
