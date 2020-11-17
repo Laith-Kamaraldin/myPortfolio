9d32676fec0a5ecfef97d4d0c6596d1ed9455c97
Laith-Kamaraldin <laithkamaral@gmail.com>
2020-11-17 19:53:05 +0000

    service worker removed

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -3,14 +3,6 @@ import Section from '../section';
 import{ repos,} from "../config.js"
 import Project from "./Project.jsx";
 import "../../scss/custom.scss";
-import * as serviceWorker from "../../serviceWorker";
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
 
 const SectionProjects =  React.forwardRef(() => {
   return (
